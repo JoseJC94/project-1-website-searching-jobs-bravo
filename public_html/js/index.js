19d647c73299a37701fa467699b52e3888f84530
David Padilla <depm100792@gmail.com>
2018-04-13 22:02:45 -0600

    cambios finales

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    consumirTrabajos();
+
 });
 
 function DameLaFechaHora() {
@@ -9,39 +9,62 @@ function DameLaFechaHora() {
     var hoy = new Date();
     var m = new Array();
     var d = new Array();
-    var an= hoy.getYear();
-    m[0]="Enero";  m[1]="Febrero";  m[2]="Marzo";
-    m[3]="Abril";   m[4]="Mayo";  m[5]="Junio";
-    m[6]="Julio";    m[7]="Agosto";   m[8]="Septiembre";
-    m[9]="Octubre";   m[10]="Noviembre"; m[11]="Diciembre";
-    document.write("Visita realizada a las "+hrs+":"+min+" (");
+    var an = hoy.getYear();
+    m[0] = "Enero";
+    m[1] = "Febrero";
+    m[2] = "Marzo";
+    m[3] = "Abril";
+    m[4] = "Mayo";
+    m[5] = "Junio";
+    m[6] = "Julio";
+    m[7] = "Agosto";
+    m[8] = "Septiembre";
+    m[9] = "Octubre";
+    m[10] = "Noviembre";
+    m[11] = "Diciembre";
+    document.write("Visita realizada a las " + hrs + ":" + min + " (");
     document.write(hoy.getDate());
     document.write(" de ");
     document.write(m[hoy.getMonth()]);
     document.write(")");
 }
 
-function consumirTrabajos() {
-    console.log("consumirTrabajos");
+function getTrabajosDesLoc() {
+    console.log("getTrabajosDesLoc");
+    $("#tableContent").remove();
     $.ajax({
         type: "GET",
         url: "https://jobs.github.com/positions.json",
         dataType: 'jsonp',
+        data: "description=" + $("#trabajo").val() + "&location=" + $("#contrasena").val(),
         success: function (data) {
             var json = data;
             var content = "";
-            jQuery.each(json, function() {
-                content = content + "<div>";
-                content = content + "<label>ID: "+ this.id +"</label>";
-                content = content + "<label>Titulo: "+ this.title +"</label>";
-                content = content + "<label>Descripcion: "+ this.description +"</label>";
-                content = content + "</div>";
+            console.log(Object.keys(json).length);
+            jQuery.each(json, function (index, val ) {
+                content = content + "<tbody id='tableContent'>";
+                content = content + "<tr>";
+                content = content + "<td>";
+                content = content + "<h4><a href=\"trabajo.html?id=" + this.id +"\">" + (index + 1) + " " + this.title + "</a></h4>";
+                content = content + "<p>";
+                content = content + "<a href=\" "+ this.company_url +"\">" + this.company + "</a>";
+                content = content + "&ndash;";
+                content = content + "<strong >" + this.type + "</strong></p>";
+                content = content + "</p>";
+                content = content + "</td>";
+                content = content + "<td>";
+                content = content + "<span>" + this.location + "</span>";
+                content = content + "&ndash;";
+                content = content + "<span>" + this.created_at + "</span>";
+                content = content + "</td>";
+                content = content + "</tr>";
+                content = content + "</tbody>";
             });
-            $("#trabajosContent").append(content);
+            $("#trabajoslist").append(content);
         },
         error: function (data) {
             console.log("error ");
         }
     });
-    
 }
+
